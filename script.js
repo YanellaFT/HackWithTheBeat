2ee75dd2e789be5560f61512443e9fa3a0da4d95
YanellaFT <yanellaft@gmail.com>
2025-08-01 22:05:31 +0000

    added Ckey w/ sound

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 /* VARIABLES */
 let piano;
+let Ckey, Dkey, Ekey, Fkey, Gkey, Akey, Bkey;
+let CkeyAud, DkeyAud, EkeyAud, FkeyAud, GkeyAud, AkeyAud, BkeyAud;
 
 
 function preload(){
   piano = loadImage("assets/piano.png");
+
+  CkeyAud = loadSound("assets/Ckey.webm");
 }
 
 
@@ -11,14 +15,28 @@ function setup() {
   createCanvas(600,500);
   background("#555661");
   image(piano, 0, 250, 600, 250);
+  //piano.layer = 1;
 
   //textFont(cursive);
   text("Play the piano and watch it make art", 100, 100);
+  
+
+  stroke("white");
+  Ckey = new Sprite(45, 445, 75, 100);
+  Ckey.color = "white";
+  //Ckey.layer = 2;
+
+
+
 }
 
 
 function draw() {
 
+  if (Ckey.mouse.pressed()) {
+    print("Ckey");
+    CkeyAud.play();
+  }
 }
 
 /* FUNCTIONS */
